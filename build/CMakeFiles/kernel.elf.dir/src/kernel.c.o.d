07cb1ff78223268fbb97a4d6c64fed52062e3aab
Shaun <shaun394@users.noreply.github.com>
2026-01-12 21:42:07 +0200

    Ignore build artifacts and logs

--- build/CMakeFiles/kernel.elf.dir/src/kernel.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/kernel.elf.dir/src/kernel.c.o: /home/shaun/myos/src/kernel.c \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint-gcc.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
